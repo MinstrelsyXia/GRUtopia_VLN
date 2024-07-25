f405593bc3448cf8522fe1ca7e735364c0176786
Virlus <maomao4171@163.com>
2024-07-25 18:46:16 +0800

    update

--- vln/src/local_nav/BEVmap.py
+++ vln/src/local_nav/BEVmap.py
@@ -67,7 +67,7 @@ class BEVMap:
         return dilation_structure
         
     ######################## update_occupancy_map ########################
-    def update_occupancy_map(self, point_cloud, robot_bottom_z, add_dilation=True, verbose = False):
+    def update_occupancy_map(self, point_cloud, robot_bottom_z, add_dilation=False, verbose = False):
         """
         Updates the occupancy map based on the new point cloud data.
         Args:
@@ -87,8 +87,7 @@ class BEVMap:
                 pcd.points = o3d.utility.Vector3dVector(pos_point_cloud)
                 downsampled_cloud = np.asarray(pcd.voxel_down_sample(voxel_size=self.voxel_size).points)
 
-                adjusted_coords = (downsampled_cloud[:, :2]/self.voxel_size + [self.quadtree_width/2, self.quadtree_height/2]).astype(int) # !!! why + quadtree_width/2？ this makes all values are greater than 500 (I see, this can make the negative points to be positive)
-                # adjusted_coords = (downsampled_cloud[:, :2]/self.voxel_size).astype(int)
+                adjusted_coords = (downsampled_cloud[:, :2]/self.voxel_size + [self.quadtree_width/2, self.quadtree_height/2]).astype(int) 
                 adjusted_coords_with_z = np.hstack((adjusted_coords, downsampled_cloud[:,2].reshape(-1,1)))
                 point_to_consider = adjusted_coords_with_z[(adjusted_coords_with_z[:, 0] < self.quadtree_width) & (adjusted_coords_with_z[:, 1] < self.quadtree_height)] # !!! there seems that 0 and 1 are reversed
 
