f48bdba4a5871fd3f7020df4247030311f3370fb
CrystalSixone <wlyvipp@163.com>
2024-08-27 21:39:14 +0800

    add sample_data_script

--- vln/parser.py
+++ vln/parser.py
@@ -14,12 +14,13 @@ from vln.src.utils.utils import dict_to_namespace
 def process_args():
     '''Init parser arguments'''
     parser = argparse.ArgumentParser(description="Main function for VLN in GRUtopia")
-    parser.add_argument("--env", default="val_seen", type=str, help="The split of the dataset", choices=['train', 'val_seen', 'val_unseen'])
+    parser.add_argument("--env", default="", type=str, help="The split of the dataset", choices=['train', 'val_seen', 'val_unseen'])
     parser.add_argument("--path_id", default=5593, type=int, help="The number of path id") # 5593
     parser.add_argument("--headless", action="store_true", default=False)
     parser.add_argument("--test_verbose", action="store_true", default=False)
     parser.add_argument("--wait", action="store_true", default=False)
     parser.add_argument("--mode", type=str, default="vis_one_path", help="The mode of the program")
+    parser.add_argument("--scan", type=str, default="", help="The target scan")
     parser.add_argument("--sim_cfg_file", type=str, default="vln/configs/sim_cfg.yaml")
     parser.add_argument("--vln_cfg_file", type=str, default="vln/configs/vln_cfg.yaml")
     parser.add_argument("--save_obs", action="store_true", default=False)
---
+++ vln/sample_data_script.py
@@ -0,0 +1,94 @@
+# Author: w61
+# Date: 2024.08.27
+''' Automatic script to sample data for VLN in isaac-sim
+'''
+import os,sys
+import gzip
+import json
+import math
+import numpy as np
+import argparse
+import yaml
+import time
+import shutil
+from collections import defaultdict
+from PIL import Image
+from copy import deepcopy
+from tqdm import tqdm
+
+# enable multiple gpus
+# import isaacsim
+# import carb.settings
+# settings = carb.settings.get_settings()
+# settings.set("/renderer/multiGPU/enabled", True)
+
+
+from grutopia.core.config import SimulatorConfig
+from grutopia.core.env import BaseEnv
+from grutopia.core.util.container import is_in_container
+from grutopia.core.util.log import log
+
+# from grutopia_extension.utils import get_stage_prim_paths
+
+import matplotlib.pyplot as plt
+
+from vln.src.dataset.data_utils import VLNDataLoader
+from vln.src.utils.utils import dict_to_namespace
+# from vln.src.local_nav.global_topdown_map import GlobalTopdownMap
+
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ISSAC_SIM_DIR = os.path.join(os.path.dirname(ROOT_DIR), "isaac-sim-4.0.0")
+sys.path.append(ISSAC_SIM_DIR)
+
+from parser import process_args
+
+import subprocess
+import sys
+
+def build_dataset():
+    ''' Build dataset for VLN
+    '''
+    vln_config, sim_config = process_args()
+    vln_datasets = {}
+    scan_file = os.path.join(vln_config.datasets.base_data_dir, "gather_data", "env_scan.json")
+    with open(scan_file, 'r') as f:
+        scan_data = json.load(f)
+    
+    return scan_data
+
+def run_scans(scan_data):
+    start_time = time.time()
+    
+    # 遍历每个数据集分割
+    for key, scans in scan_data.items():
+        print(f"处理数据集分割: {key}，包含 {len(scans)} 个扫描")
+        
+        # 使用 tqdm 显示进度条
+        for scan in tqdm(scans, desc=f"Processing scans in {key}", unit="scan"):
+            print(f"正在处理scan: {scan}")
+            
+            # 构建命令
+            command = [sys.executable, 'vln/main.py', '--scan', scan, '--env', key,
+                       '--headless', 
+                       '--vln_cfg_file', 'vln/configs/vln_extract_data_script.yaml', 
+                       '--sim_cfg_file', 'vln/configs/sample_episodes_sim_cfg.yaml']
+            
+            try:
+                # 执行main.py，并等待其完成
+                subprocess.run(command, check=True)
+                print(f"Scan {scan} 处理完成")
+            except subprocess.CalledProcessError as e:
+                print(f"处理scan {scan} 时发生错误: {e}")
+            
+            print("---")
+    
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(f"所有扫描处理完成，总耗时: {total_time:.2f}秒")
+
+
+if __name__ == "__main__":
+    scan_data = build_dataset()
+
+    run_scans(scan_data)
