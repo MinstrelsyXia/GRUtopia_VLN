4c6ddf9553c7f6076727251ddf7cb0369c73542e
CrystalSixone <wlyvipp@163.com>
2024-12-07 23:45:18 +0800

    update eval

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,5 +1,5 @@
 NAME=w61_grutopia
-sudo docker run -d --name ${NAME} -it --rm --gpus='"device=0,1,2,3"' --network host \
+sudo docker run -d --name ${NAME} -it --rm --gpus='"device=4,5,6,7"' --network host \
      -e "ACCEPT_EULA=Y" \
      -e "PRIVACY_CONSENT=Y" \
      -e "WEBUI_HOST=${WEBUI_HOST}" \
