1aecabafbafac0ef37ee6bff3fd4fb7a22f7c5db
CrystalSixone <wlyvipp@163.com>
2024-12-20 09:35:01 +0800

    update

--- vln/scripts/run_eval.sh
+++ vln/scripts/run_eval.sh
@@ -8,17 +8,21 @@ attempt=1
 while [ $attempt -le $max_attempts ]; do
     if [ "$1" == "--val_seen" ]; then
         flags="
-            --exp-config vln/configs/train/cma_dp_eval_val_seen.yaml
+            --exp-config vln/configs/train/cma_dp_eval.yaml
             --run-type eval
             --headless
             --test_verbose
+            EVAL.SPLIT ['val_unseen']
+            EVAL.re_eval False
         "
     elif [ "$1" == "--val_unseen" ]; then
         flags="
-            --exp-config vln/configs/train/cma_dp_eval_val_unseen.yaml
+            --exp-config vln/configs/train/cma_dp_eval_val.yaml
             --run-type eval
             --headless
             --test_verbose
+            EVAL.SPLIT ['val_unseen']
+            EVAL.re_eval False
         "
     elif [ "$1" == "--train" ]; then
         flags="
@@ -26,6 +30,8 @@ while [ $attempt -le $max_attempts ]; do
             --run-type eval
             --headless
             --test_verbose
+            EVAL.SPLIT ['train']
+            EVAL.re_eval False
         "
     fi
 
