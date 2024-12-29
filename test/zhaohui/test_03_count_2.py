1c0dc752deffaa040a3af9ab4a6889c41c9729af
zhaohui <zhaohui@pjlab.org.cn>
2024-12-29 19:13:26 +0800

    update

--- test/zhaohui/test_03_count_2.py
+++ test/zhaohui/test_03_count_2.py
@@ -27,16 +27,17 @@ def get_split_map():
                 split_map[path_key] = split_data_type
     return split_map
 
-ranks =[8,9,10,11,12,13,14,15] 
+ranks =list(range(0,8))
 ckpt_name="ckpt.44"
 project_path = '/ssd/zhaohui/workspace/w61_grutopia_1220'
 name = '20241229_sample_episodes'
 # split = 'val_seen'
-split = 'val_unseen'
-# split = ''
+# split = 'val_unseen'
+split = ''
 lmdb_path = project_path + f'/data/sample_episodes/{name}'
 database_read = lmdb.open(f"{lmdb_path}/sample_data.lmdb", readonly=True, lock=False)
-split_map = get_split_map()
+if split != '':
+    split_map = get_split_map()
 
 all_rank_total_count=0
 all_rank_finist_count=0
