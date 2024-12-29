fa57183c3d907344a96db7e6d5e9c685a919afb0
zhaohui <zhaohui@pjlab.org.cn>
2024-12-29 18:56:36 +0800

    update

--- test/zhaohui/test_03_count_2.py
+++ test/zhaohui/test_03_count_2.py
@@ -1,15 +1,42 @@
 import lmdb
 import msgpack_numpy
+from vln.src.dataset.data_utils_multi_env import load_gather_data
+from vln.src.utils.utils import Config
 
 def generate_result_key(ckpt_name, path_key):
     return f"eval_{ckpt_name}_{path_key}"
+
+def get_split_map():
+    split_map={}
+    base_data_dir = f'{project_path}/data/datasets/R2R_VLNCE_v1-3_corrected'
+    mp3d_data_dir = f"{project_path}/../Matterport3D/data/v1/scans"
+    args_dict = {
+        "datasets":{
+            "mp3d_data_dir":mp3d_data_dir,
+            "base_data_dir":base_data_dir,
+        }
+    }
+    split_data_types=['val_unseen','val_seen']
+    for split_data_type in split_data_types:
+        load_data_map, _ = load_gather_data(Config(args_dict), split_data_type, filter_same_trajectory=False, filter_stairs=True)
+        for scan,path_list in load_data_map.items():
+            for path in path_list:
+                trajectory_id = path['trajectory_id']
+                episode_id = path['episode_id']
+                path_key = f"{trajectory_id}_{episode_id}"
+                split_map[path_key] = split_data_type
+    return split_map
 
 ranks =[0,1,2,3,4,5,6,7] 
 ckpt_name="ckpt.44"
 project_path = '/ssd/zhaohui/workspace/w61_grutopia_1220'
 name = '20241229_sample_episodes'
+# split = 'val_seen'
+# split = 'val_unseen'
+split = ''
 lmdb_path = project_path + f'/data/sample_episodes/{name}'
 database_read = lmdb.open(f"{lmdb_path}/sample_data.lmdb", readonly=True, lock=False)
+split_map = get_split_map()
 
 all_rank_total_count=0
 all_rank_finist_count=0
@@ -51,6 +78,9 @@ for rank in ranks:
     success_count=0
     for scan,path_key_list in value.items():
         for path_key in path_key_list:
+            if split != '':
+                if split_map[path_key] != split:
+                    continue
             total_count += 1
             info_key = generate_result_key(ckpt_name,path_key)
             with database_read.begin() as txn:
@@ -67,11 +97,12 @@ for rank in ranks:
                 success = info_value['success']
                 if success > 0:
                     success_count += 1
-    print(f"[rank:{rank}][ {finist_count} / {total_count}][success:{success_count}]:{count_map}")
+    print(f"[rank:{rank}][split:{split}][ {finist_count} / {total_count}][success:{success_count}]:{count_map}")
     all_rank_total_count +=total_count
     all_rank_finist_count +=finist_count
     all_rank_success_count +=success_count
     for k,v in count_map.items():
         all_rank_count_map[k] = all_rank_count_map[k] + v
 
-print(f"[all_rank][ {all_rank_finist_count} / {all_rank_total_count}][success:{all_rank_success_count}]:{all_rank_count_map}")
+print(f"[all_rank][split:{split}][ {all_rank_finist_count} / {all_rank_total_count}][success:{all_rank_success_count}]:{all_rank_count_map}")
+database_read.close()
