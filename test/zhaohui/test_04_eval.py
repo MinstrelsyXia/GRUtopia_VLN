32dcdabcac3e58d900d5e0f32570c5be5d24be2a
yaji07 <2643980756@qq.com>
2025-01-02 19:41:22 +0800

    bugfix

--- test/zhaohui/test_04_eval.py
+++ test/zhaohui/test_04_eval.py
@@ -1,4 +1,4 @@
-from vln.src.v2.env.discrete_eval import DiscreteEvalSingleScanEnv
+from vln.src.v2.envs.discrete_eval import DiscreteEvalSingleScanEnv
 from vln.src.v2.dataloader.eval import EvalPathKeyDataloader
 from grutopia.core.config import SimulatorConfig
 from vln.src.dataset.data_utils_multi_env import load_scene_usd
