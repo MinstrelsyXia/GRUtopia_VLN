59b6084c60bf70bfbe6f66c63074f72cc68581f2
CrystalSixone <wlyvipp@163.com>
2025-01-13 19:40:30 +0800

    update cma dagger train

--- vln/scripts/run_train_slurm.sh
+++ vln/scripts/run_train_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=cmaFlash         # 作业名称
-#SBATCH --output=logs/%x_%j.out          # 标准输出文件路径 (%j 会被替换为作业ID)
-#SBATCH --error=logs/%x_%j.err           # 标准错误文件路径
+#SBATCH --job-name=cmaDaggerTrain         # 作业名称
+#SBATCH --output=logs/%j_%x.out          # 标准输出文件路径
+#SBATCH --error=logs/%j_%x.err           # 标准错误文件路径
 #SBATCH --gres=gpu:1                 # GPU请求
 #SBATCH --cpus-per-task=6            # 每个任务的CPU核心数
 #SBATCH --partition=smartbot              # 使用GPU分区
@@ -10,6 +10,8 @@
 # 创建日志目录
 mkdir -p logs
 
+module load anaconda/2024.02
+module unload tensorboard
 source activate grutopia_train
 
 export MAGNUM_LOG=quiet
