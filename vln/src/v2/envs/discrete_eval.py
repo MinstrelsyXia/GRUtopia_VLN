80fd04d1a372da4f465bd82415716beab38403b5
CrystalSixone <wlyvipp@163.com>
2025-02-17 17:46:50 +0800

    ready to merge navid_eval

--- vln/src/v2/envs/discrete_eval.py
+++ vln/src/v2/envs/discrete_eval.py
@@ -66,10 +66,11 @@ class DiscreteEvalSingleScanEnv(BaseSingleScanEnv):
         else:
             self.use_clip_encoders = False
         
+        self.use_bert = False
+        self.bert_tokenizer = None
+        self.is_clip_long = False
+
         if self.use_clip_encoders:
-            self.use_bert = False
-            self.bert_tokenizer = None
-            self.is_clip_long = False
             if self.eval_config.MODEL.TEXT_ENCODER.type == 'roberta':
                 self.bert_tokenizer = BertTokenizer(
                     max_length=self.eval_config.MODEL.INSTRUCTION_ENCODER.max_length,
@@ -206,7 +207,7 @@ class DiscreteEvalSingleScanEnv(BaseSingleScanEnv):
                     if self.use_clip_encoders:
                         actions, rnn_states, _ = self.policy(batch)
                     else:
-                        actions, rnn_states = self.policy(batch)
+                        actions, rnn_states, _ = self.policy(batch)
                 prev_actions.copy_(actions)
                 if self.eval_config.EVAL.ACTION == 'descrete':
                     for bs_i, a in enumerate(actions):
