e1ffcf72a956363ba1426693ce76127c5630e2e8
CrystalSixone <wlyvipp@163.com>
2025-02-19 11:18:06 +0800

    update flash eval

--- scripts/slurm/one_slurm.sh
+++ scripts/slurm/one_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J 20250217_aliengo_ckpt55_eval
+#SBATCH -J 20250219_h1_cmaSOTA_flash_eval
 #SBATCH -o slurm_logs/%j_%x.out 
 #SBATCH -e slurm_logs/%j_%x.err 
 #SBATCH -p gpu_4090  
--- vln/src/v2/envs/discrete_flash_eval.py
+++ vln/src/v2/envs/discrete_flash_eval.py
@@ -18,6 +18,9 @@ import msgpack_numpy
 from vln.src.models.init_policy import initialize_policy
 from vln.src.v2.util.data_collector import DataCollector
 
+from vln.src.models.LongCLIP.model import longclip
+from vln.src.models.utils.bert_token import BertTokenizer
+
 class DiscreteFlashEvalSingleScanEnv(BaseSingleScanEnv):
     
     def __init__(
@@ -34,7 +37,7 @@ class DiscreteFlashEvalSingleScanEnv(BaseSingleScanEnv):
             ckpt_name,
         ):
         super().__init__(
-            robot_name,
+            robot_name=robot_name,
             sim_config=sim_config,
             scene_asset_path=scene_asset_path,
             start_position=start_position,
@@ -45,7 +48,9 @@ class DiscreteFlashEvalSingleScanEnv(BaseSingleScanEnv):
         #TODO:
         self.device = torch.device("cuda", 0)
         self.eval_config = eval_config
-        self.max_step=500
+        #TODO:
+        self.per_action_max_step=1500
+        self.max_step=25000
         self.lmdb_path = lmdb_path
         self.ckpt_name = ckpt_name
         policy, _, _, _ = initialize_policy(
@@ -57,6 +62,27 @@ class DiscreteFlashEvalSingleScanEnv(BaseSingleScanEnv):
             action_stats=None,
         )
         self.policy = policy
+
+        if self.eval_config.MODEL.policy_name == "CMA_CLIP_Policy":
+            self.use_clip_encoders = True
+        else:
+            self.use_clip_encoders = False
+        
+        self.use_bert = False
+        self.bert_tokenizer = None
+        self.is_clip_long = False
+        if self.use_clip_encoders:
+            if self.eval_config.MODEL.TEXT_ENCODER.type == 'roberta':
+                self.bert_tokenizer = BertTokenizer(
+                    max_length=self.eval_config.MODEL.INSTRUCTION_ENCODER.max_length,
+                    load_model=self.eval_config.MODEL.INSTRUCTION_ENCODER.load_model,
+                    device=self.device
+                )
+                self.use_bert = True
+            elif self.eval_config.MODEL.TEXT_ENCODER.type == 'clip-long':
+                self.bert_tokenizer = longclip.tokenize
+                self.use_bert = True
+                self.is_clip_long = True
 
 
     def topdown_snapshot(self):
@@ -84,6 +110,7 @@ class DiscreteFlashEvalSingleScanEnv(BaseSingleScanEnv):
         scan = self.dataloader.target_scan
         progress_log_util.init(scan, len(eval_path_key_list), rank=self.dataloader.rank)
         progress_log_util.progress_logger.info(f"start eval scan: {scan}, total_path:{len(eval_path_key_list)}")
+
         data_collector = DataCollector(self.dataloader.lmdb_path,self.dataloader.rank)
 
         self.policy.eval()
@@ -105,12 +132,22 @@ class DiscreteFlashEvalSingleScanEnv(BaseSingleScanEnv):
 
             # map_info = self.topdown_snapshot()
             robot_position, robot_rotation = self.task.get_robot_poses_without_offset()
-            observations = get_obs(self.env, data['instruction'],robot_position,robot_rotation)
+
+            if 'sub_instruction' in data:
+                sub_instr = data['sub_instruction']
+                sub_instr_tokens = data['sub_instruction_tokens']
+                max_instr_len = 100
+            else:
+                sub_instr = None
+                sub_instr_tokens = None
+                max_instr_len = 200
+            observations = get_obs(self.env, data['instruction'],robot_position,robot_rotation, sub_instr, sub_instr_tokens, robot_name=self.robot_name)
             observations = extract_instruction_tokens(
                 observations, 
                 #TODO:
-                bert_tokenizer=None,
-                is_clip_long=False
+                bert_tokenizer=self.bert_tokenizer,
+                is_clip_long=self.is_clip_long,
+                max_instr_len=max_instr_len
             )
             observations = batch_obs(observations, self.device)
             observations["steps"] = torch.from_numpy(np.array([0])).to(self.device)
@@ -154,8 +191,22 @@ class DiscreteFlashEvalSingleScanEnv(BaseSingleScanEnv):
                     'prev_actions': prev_actions,
                     'masks': not_done_masks
                 }
+
+                if self.use_clip_encoders:
+                    batch.update({
+                        "need_img_extraction": True,
+                        "img_mod": self.eval_config.MODEL.IMAGE_ENCODER.RGB.img_mod,
+                        'proj': self.eval_config.MODEL.IMAGE_ENCODER.RGB.rgb_proj,
+                        'process_images': True,
+                        'need_txt_extraction': True,
+                        "depth_return_x_before_fc": False
+                    })
+
                 with torch.no_grad():
-                    actions, rnn_states = self.policy(batch)
+                    if self.use_clip_encoders:
+                        actions, rnn_states, _ = self.policy(batch)
+                    else:
+                        actions, rnn_states, _ = self.policy(batch)
                 prev_actions.copy_(actions)
                 action = actions[0].item()
                 executor = FlashActionExecutor(
@@ -173,11 +224,16 @@ class DiscreteFlashEvalSingleScanEnv(BaseSingleScanEnv):
                 statistic_info = executor.statistic_info
                 statistic_info.policy_step +=1
 
+                if 'sub_instruction' in data: # MLANet
+                    max_instr_len = 100
+                else:
+                    max_instr_len = 200
                 outputs_dict = extract_instruction_tokens(
                     outputs_dict,
                     #TODO: 
-                    bert_tokenizer=None,
-                    is_clip_long=False
+                    bert_tokenizer=self.bert_tokenizer,
+                    is_clip_long=self.is_clip_long,
+                    max_instr_len=max_instr_len
                 )
                 observations = batch_obs(outputs_dict, self.device)
                 observations["steps"] = torch.from_numpy(np.array([0])).to(self.device)
